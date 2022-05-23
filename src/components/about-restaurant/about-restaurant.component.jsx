0ae2e7071066277341350ae83f97c5ae58dc8804
oniikovec <oniikovec@gmail.com>
2022-05-23 23:12:42 +0200

    added photoswipe to gallery-preview

---
+++ src/components/about-restaurant/about-restaurant.component.jsx
@@ -0,0 +1,12 @@
+import {  } from './about-restaurant.styles'
+
+const AboutRestaurant = () => {
+
+  return (
+    <>
+      <h1>THIS IS ABOUT REST MADAFAKA</h1>
+    </>
+  )
+}
+
+export default AboutRestaurant
