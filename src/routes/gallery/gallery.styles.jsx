3106e98d78461577666056b886ad0dd285c9773d
oniikovec <oniikovec@gmail.com>
2022-05-24 17:53:44 +0200

    middle of remaking gallery from querries to autofill

--- src/routes/gallery/gallery.styles.jsx
+++ src/routes/gallery/gallery.styles.jsx
@@ -38,7 +38,7 @@ export const GalleryHeader = styled.div`
     display: grid;
     align-items: center;
     justify-items: center;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
     grid-gap: 20px;
   `
 
